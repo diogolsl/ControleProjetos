17794b0d6e35122b8d2115fd8e442854fd30a1d0
diogolsl <diogolucasdelucena@gmail.com>
2026-04-22 15:27:23 -0300

    Rodando os novos testes

--- src/main/java/trabalhoA2/Principal.java
+++ src/main/java/trabalhoA2/Principal.java
@@ -2,6 +2,8 @@ package trabalhoA2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Principal{
@@ -10,4 +12,8 @@ public class Principal{
 		SpringApplication.run(Principal.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- src/main/java/trabalhoA2/service/FeriadoService.java
+++ src/main/java/trabalhoA2/service/FeriadoService.java
@@ -10,26 +10,24 @@ import java.util.List;
 
 @Service
 public class FeriadoService {
+
     private final RestTemplate restTemplate;
     private static final String BRASIL_API_URL = "https://brasilapi.com.br/api/feriados/v1/";
 
-    public FeriadoService() {
-        this.restTemplate = new RestTemplate();
+    public FeriadoService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public List<FeriadoResponseDTO> listarFeriadosPorAno(String ano) {
         String url = BRASIL_API_URL + ano;
-
         try {
             FeriadoResponseDTO[] feriadosArray = restTemplate.getForObject(url, FeriadoResponseDTO[].class);
-
             if (feriadosArray != null) {
                 return Arrays.asList(feriadosArray);
             }
         } catch (Exception e) {
-            System.err.println("Erro ao buscar feriados na Brasil API: " + e.getMessage());
+            System.err.println("Erro ao buscar feriados: " + e.getMessage());
         }
-
         return Collections.emptyList();
     }
 }
